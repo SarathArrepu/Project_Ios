49e5135b57daccf07fdd026f0b5d2bd2df9a5765
SARATH ARREPU <89147362+SarathArrepu@users.noreply.github.com>
2022-04-23 15:06:26 -0500

    Add files via upload

---
+++ MovieApp/MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Sai Lavanya Peddinti on 4/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
